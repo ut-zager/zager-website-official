2168f1ad0c857875c30c8595d1ff3b4e537314b3
sahil sharma <sahilsharma06478@gmail.com>
2025-02-23 15:40:40 +0530

    Client page updated

--- Frontend/src/Components/Clients.jsx
+++ Frontend/src/Components/Clients.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
-import './ui/Clients.css';
+import './ui/Clients.css'; // Import the CSS file
+
 const Clients = () => {
   return (
-    <div className="flex justify-center items-center pt-5">
-      <div className="h-[60vh] w-[80vw] flex flex-col items-center justify-center">
-        <h1 className="text-center text-2xl font-bold mb-4">Our Clients</h1>
-        <div className="relative flex items-center justify-center h-full bg-white bg-opacity-10 rounded-[50px_150px] w-full overflow-hidden">
-          <div className="absolute left-0 top-0 z-10 h-full w-[10%] bg-gradient-to-r from-black to-transparent"></div>
-          <div className="absolute right-0 top-0 z-10 h-full w-[10%] bg-gradient-to-l from-black to-transparent"></div>
+    <div className="clients-container">
+      <div className="clients-box">
+      <h4 style={{ fontWeight: "700", fontSize: "2.5rem", color:"#ffbe00", textShadow:"0 0 12px rgba(238, 201, 115, 0.753)", marginBottom:"20px" }} className="text-3xl font-bold text-center text-[#ffbe00] mb-4">Our Clients</h4>
+        <div className="clients-carousel">
+          <div className="gradient-left"></div>
+          <div className="gradient-right"></div>
           
-          <div className="flex animate-scroll whitespace-nowrap space-x-4"> {/* Added space-x-4 for spacing */}
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Ffront.png?alt=media&token=a649eb27-4375-4244-a58e-6195da276306" alt="" className="h-[10vh] max-h-[60px] object-contain" />
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Ffusion.png?alt=media&token=95dea704-ce1c-4b1b-87c7-60d7c7896d7d" alt="" className="h-[10vh] max-h-[60px] object-contain" />
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Flpc.png?alt=media&token=c7236b6b-4b59-40e5-8b0e-e0bbb593955b" alt="" className="h-[10vh] max-h-[60px] object-contain" />
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Fskygym.png?alt=media&token=164f00db-f726-4625-a24b-f037271a7aac" alt="" className="h-[10vh] max-h-[60px] object-contain" />
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Fnidaanmonopng.PNG.png?alt=media&token=2a4349bf-2714-4b7f-920a-70cede7ae914" alt="" className="h-[10vh] max-h-[60px] object-contain" />
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2FLogo4x.png?alt=media&token=2620ba16-f280-4fd3-90a6-0f4a1de2d2f3" alt="" className="h-[10vh] max-h-[60px] object-contain" />
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Fargo.png?alt=media&token=29f0c24f-2f7b-4d20-8aba-1e0d36f26d0f" alt="" className="h-[10vh] max-h-[60px] object-contain" />
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Farise.jpg?alt=media&token=88c4161d-8988-419c-970a-53ef8169b8ae" alt="" className="h-[10vh] max-h-[60px] object-contain" />
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Ffinedine.png?alt=media&token =586d67bc-6228-4b23-924b-9f413505f565" alt="" className="h-[10vh] max-h-[60px] object-contain" />
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Fhygiene.jpg?alt=media&token=b75d473b-f842-41f4-8d42-a00e50bfd238" alt="" className="h-[10vh] max-h-[60px] object-contain" />
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Flpc.png?alt=media&token=c7236b6b-4b59-40e5-8b0e-e0bbb593955b" alt="" className="h-[10vh] max-h-[60px] object-contain" />
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Fmomos_nation2.png?alt=media&token=636db2fd-46f3-47b7-a0f6-aea8c949317e" alt="" className="h-[10vh] max-h-[60px] object-contain" />
-            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Frevive.png?alt=media&token=4bddc43b-d024-4eba-9b55-c818aad9d86d" alt="" className="h-[10vh] max-h-[60px] object-contain" />
+          <div className="scroll-container">
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Ffront.png?alt=media&token=a649eb27-4375-4244-a58e-6195da276306" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Ffusion.png?alt=media&token=95dea704-ce1c-4b1b-87c7-60d7c7896d7d" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Flpc.png?alt=media&token=c7236b6b-4b59-40e5-8b0e-e0bbb593955b" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Fskygym.png?alt=media&token=164f00db-f726-4625-a24b-f037271a7aac" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Fnidaanmonopng.PNG.png?alt=media&token=2a4349bf-2714-4b7f-920a-70cede7ae914" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2FLogo4x.png?alt=media&token=2620ba16-f280-4fd3-90a6-0f4a1de2d2f3" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Fargo.png?alt=media&token=29f0c24f-2f7b-4d20-8aba-1e0d36f26d0f" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Farise.jpg?alt=media&token=88c4161d-8988-419c-970a-53ef8169b8ae" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Ffinedine.png?alt=media&token=586d67bc-6228-4b23-924b-9f413505f565" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Fhygiene.jpg?alt=media&token=b75d473b-f842-41f4-8d42-a00e50bfd238" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Flpc.png?alt=media&token=c7236b6b-4b59-40e5-8b0e-e0bbb593955b" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Fmomos_nation2.png?alt=media&token=636db2fd-46f3-47b 7-a0f6-aea8c949317e" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Frevive.png?alt=media&token=4bddc43b-d024-4eba-9b55-c818aad9d86d" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Ffront.png?alt=media&token=a649eb27-4375-4244-a58e-6195da276306" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Ffusion.png?alt=media&token=95dea704-ce1c-4b1b-87c7-60d7c7896d7d" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Flpc.png?alt=media&token=c7236b6b-4b59-40e5-8b0e-e0bbb593955b" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Fskygym.png?alt=media&token=164f00db-f726-4625-a24b-f037271a7aac" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/zager-website.appspot.com/o/companiesImages%2Fnidaanmonopng.PNG.png?alt=media&token=2a4349bf-2714-4b7f-920a-70cede7ae914" alt="" />
           </div>
         </div>
       </div>
